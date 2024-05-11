060bd9850fe6144d67df5da792b40ff6de94df1d
Raphael Sá <dev.raphaelsa@gmail.com>
2024-05-11 14:06:33 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# freecad-preferences
+# Freecad Preferences
+
+Custom preference for FreeCAD
+
+## Dependencies
+
+- Glass
+- OpenDark
+- PieMenu
+
+## Tested
+
+- [x] Linux
+- [ ] Windows
+- [ ] MacOSX
+
+## Screenshots
+
+Part Design
+![Part Design](assets/screenshot-partdesign.png)
+Sketcher
+![Sketcher](assets/screenshot-sketcher.png)
+
+## Install
+
+- Linux
+
+```shell
+chmod +x preferences.sh
+./preferences.sh install
+```
+
+- Windows
+
+  `To do`
+
+- MacOSX
+
+  `To do`
+
+## Uninstall
+
+- Linux
+
+```shell
+chmod +x preferences.sh
+./preferences.sh uninstall
+```
+
+- Windows
+
+  `To do`
+
+- MacOSX
+
+  `To do`
---
+++ preferences.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+CONFIG_DIR=~/.config/FreeCAD
+LOCAL_SHARE_DIR=~/.local/share/FreeCAD
+SUFIX=.BKP
+
+clean(){
+  echo "- Removing files..."
+  rm -rf $CONFIG_DIR
+  rm -rf $LOCAL_SHARE_DIR
+}
+
+backup_preferences(){
+  echo "- Backing up preferences..."
+  [ -d  $CONFIG_DIR ] && [ ! -d $CONFIG_DIR$SUFIX ] && mv $CONFIG_DIR $CONFIG_DIR$SUFIX
+  [ -d  $LOCAL_SHARE_DIR ] && [ ! -d $LOCAL_SHARE_DIR$SUFIX ] && mv $LOCAL_SHARE_DIR $LOCAL_SHARE_DIR$SUFIX
+}
+
+restore_preferences(){
+  echo "- Restoring preferences..."
+  [ -d  $CONFIG_DIR$SUFIX ] && mv $CONFIG_DIR$SUFIX $CONFIG_DIR
+  [ -d  $LOCAL_SHARE_DIR$SUFIX ] && mv $LOCAL_SHARE_DIR$SUFIX $LOCAL_SHARE_DIR
+}
+
+install(){
+  backup_preferences
+  clean
+  echo "- Installing files..."
+  cp -r ./preferences/.config ~/
+  cp -r ./preferences/.local ~/
+}
+
+uninstall(){
+  clean
+  restore_preferences
+}
+
+case $1 in
+  install)
+    install
+    ;;
+  uninstall)
+    uninstall
+    ;;
+  clean)
+    clean
+    ;;
+  *)
+    echo "Usage: $0 {install|uninstall|clean}"
+    exit 1
+    ;;
+esac
